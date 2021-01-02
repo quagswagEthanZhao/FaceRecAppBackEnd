c96d9c7f531ec9e30fafb2221f901cc2e3779ff0
quagswagEthanZhao <yixinzhaousu@gmail.com>
2021-01-01 21:33:23 -0700

    Connecting database

--- controller/userController.js
+++ controller/userController.js
@@ -1,29 +1,58 @@
 const users = require('../model/fakeUserData');
+const knex = require('knex');
+const bcrypt = require('bcrypt');
+const postgres = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'admin',
+        database: 'detectapp'
+    },
+});
 module.exports = {
-    signin: (req, res, next) => {
+    signin: async (req, res, next) => {
         const { email, password } = req.body;
-        const user = users.find(u => u.email === email);
-        // console.log(user);
-        // console.log(`databasepass${user.password} inputpass${password}`)
-        if (!user) return res.status(404).json({ error: true, message: 'Email is not registered' });
-        if (user.password !== password) return res.status(404).json({ error: true, message: 'Password not match' });
-        res.status(200).json({ error: false, message: `You are logged in`, currentUser: user });
+        try {
+            const resoult = await postgres.select('*').from('login').where('email', email);
+            const userLogin = resoult[0];
+            if (!userLogin) return res.status(404).json({ error: true, message: 'Email is not registered' });
+            const isValid = await bcrypt.compare(password, userLogin.hash);
+            if (!isValid) return res.status(404).json({ error: true, message: 'The password is not match' });
+            postgres.select('*').from('users').where('email', email)
+                .then(user => res.status(200).json({ error: false, message: `You are logged in!`, currentUser: user[0] }))
+                .catch(error => res.status(400).json({ error: true, message: 'Internal error unable to register right now', status: error }));
+        } catch (error) {
+            res.status(400).json({ error: true, message: 'Internal error cannot login now' });
+        }
     },
 
-    register: (req, res, next) => {
+    register: async (req, res, next) => {
         const { email, name, password } = req.body;
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
         const emailError = users.find(user => user.email === email);
         if (emailError) return res.status(400).json({ error: true, message: 'Email is already being registered' });
-        //Fake database query 
-        users.push({
-            id: String(users.length + 1),
-            name: name,
-            email: email,
-            password: password,
-            entries: 0,
-            joinedDate: new Date()
+        postgres.transaction(trx => {
+            trx.insert({
+                hash: hashedPassword,
+                email: email
+            })
+                .into('login')
+                .returning('email')
+                .then(loginEmail => {
+                    trx('users')
+                        .returning('*')
+                        .insert({
+                            email: loginEmail[0],
+                            name: name,
+                            joined: new Date()
+                        })
+                        .then(user => res.json({ error: false, message: `Registered for user: ${user[0].name}`, currentUser: user[0] }))
+                        .then(trx.commit)
+                        .catch(trx.rollback);
+                })
         })
-        res.json({ error: false, message: `Registered for ${users[users.length - 1].name}` });
     },
     findSingleUser: (req, res, next) => {
         const id = req.params.id;
@@ -37,18 +66,14 @@ module.exports = {
             message: user
         });
     },
-    updateUserEntre: (req, res, next) => {
+    updateUserEntre: async (req, res, next) => {
         const id = req.body.id;
-        const user = users.find(user => user.id === id);
-        if (!user) return res.status(404).json({
-            error: true,
-            message: 'User Not Find'
-        });
-        user.entries++;
-        res.status(200).json({
-            error: false,
-            message: 'updated!',
-            currentUser: user
-        });
+        try {
+            const user = await postgres('users').where('id', '=', id).increment('entries').returning('*');
+            if (!user) return res.status(404).json({ error: true, message: 'Fail to update the user' });
+            res.status(200).json({ error: false, message: 'Entries updated', currentUser: user[0] });
+        } catch (error) {
+            res.status(400).json({ error: true, message: 'Internal error' });
+        }
     }
 }
--- model/fakeUserData.js
+++ model/fakeUserData.js
@@ -1,21 +1,5 @@
 const users = [
     {
-        id: '1',
-        name: 'Joine',
-        email: 'joine@email.com',
-        password: '12345',
-        entries: 0,
-        joinedDate: new Date()
-    },
-    {
-        id: '2',
-        name: 'Roger',
-        email: 'roger@email.com',
-        password: '12345',
-        entries: 0,
-        joinedDate: new Date()
-    },
-    {
         id: '3',
         name: 'sam',
         email: 'sam@email.com',
